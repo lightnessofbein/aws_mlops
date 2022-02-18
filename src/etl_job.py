7da0c0aa6658c000842d26bfbaddaae7112973e6
lightnessofbein <lightnessofbein@gmail.com>
2022-02-18 14:06:02 +0200

    code smell fixed

--- src/etl_job.py
+++ src/etl_job.py
@@ -2,7 +2,7 @@
 
 import sys
 from typing import Any
-# from awsglue.transforms import *
+
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
