9652f246bba598dffb08cfd4eda2209a8c5d33f5
lightnessofbein <lightnessofbein@gmail.com>
2022-06-07 20:31:16 +0300

    small fix

--- data_extraction_pipe/config.py
+++ data_extraction_pipe/config.py
@@ -4,4 +4,4 @@ IAM_ROLE: str = f'service-role/AWSGlueServiceRole-{IAM_USERNAME}-mlops-retrain'
 OWNERSHIP_TAG: dict[str, str] = {'owner': IAM_USERNAME}
 
 
-GITHUB_OAUTH_TOKEN = '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',  # TODO: add auth token securily
+GITHUB_OAUTH_TOKEN: str = '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'  # TODO: add auth token securily
