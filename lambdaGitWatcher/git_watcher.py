1f4631f19189b7c35a2de9fc5d032229c84da680
lightnessofbein <lightnessofbein@gmail.com>
2022-06-08 15:30:50 +0300

    minor naming fix

--- lambdaGitWatcher/git_watcher.py
+++ lambdaGitWatcher/git_watcher.py
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
     if len(folderName) > 0:
         for singleFolderName in folderName:
             # Codepipeline name is user-foldername-job.
-            start_code_pipeline(f'sfeda-{singleFolderName}-job')
+            start_code_pipeline(f'sfeda-{singleFolderName}')
 
     return {
         'statusCode': 200,
