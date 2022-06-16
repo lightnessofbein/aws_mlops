3a60a4d9b66d1739911c2cbd99115ef225d25a5d
lightnessofbein <lightnessofbein@gmail.com>
2022-06-16 15:56:38 +0300

    added dotenv secrets

--- data_extraction_pipe/config.py
+++ data_extraction_pipe/config.py
@@ -1,7 +1,13 @@
+import os 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 IAM_USERNAME = 'sfeda'
 REGION: str = 'us-west-1'
 IAM_ROLE: str = f'service-role/AWSGlueServiceRole-{IAM_USERNAME}-mlops-retrain'
 OWNERSHIP_TAG: dict[str, str] = {'owner': IAM_USERNAME}
 
 
-GITHUB_OAUTH_TOKEN: str = ' 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'  # TODO: add auth token securily
+GITHUB_OATH_TOKEN: str = os.environ.get('GITHUB_OATH_TOKEN', '')
