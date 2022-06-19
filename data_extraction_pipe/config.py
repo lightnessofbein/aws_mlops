d677f54476439ee0c73a26828e9ff9f61b19b121
lightnessofbein <lightnessofbein@gmail.com>
2022-06-19 23:12:01 +0300

    fixed naming

--- data_extraction_pipe/config.py
+++ data_extraction_pipe/config.py
@@ -10,4 +10,4 @@ IAM_ROLE: str = f'service-role/AWSGlueServiceRole-{IAM_USERNAME}-mlops-retrain'
 OWNERSHIP_TAG: dict[str, str] = {'owner': IAM_USERNAME}
 
 
-GITHUB_OATH_TOKEN: str = os.environ.get('GITHUB_OATH_TOKEN', '')
+GITHUB_OAUTH_TOKEN: str = os.environ.get('GITHUB_OATH_TOKEN', '')
