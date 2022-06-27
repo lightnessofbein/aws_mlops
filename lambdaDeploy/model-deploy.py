952391a936cc23f0a968c1c4549570799b76e356
lightnessofbein <lightnessofbein@gmail.com>
2022-06-27 21:51:51 +0300

    model-deploy small fixes

--- lambdaDeploy/model-deploy.py
+++ lambdaDeploy/model-deploy.py
@@ -1,23 +1,23 @@
 import boto3
 
-sagemaker = boto3.client('sagemaker')
-s3 = boto3.resource('s3')
-EXECUTION_ROLE = 'arn:aws:iam::508741970469:role/service-role/sfeda-lambda-role-0za8fvd6'
-INSTANCE_TYPE = 'ml.m5.2xlarge'
-model_package_group_name = 'sfeda-model-package'
-endpoint = 'sfeda-deployed'
+sagemaker = boto3.client("sagemaker")
+s3 = boto3.resource("s3")
+EXECUTION_ROLE = "arn:aws:iam::508741970469:role/service-role/sfeda-lambda-role-0za8fvd6"
+INSTANCE_TYPE = "ml.m5.2xlarge"
+model_package_group_name = "sfeda-model-package"
+endpoint = "sfeda-deployed"
 
 
 DEPLOY = False
 APPROVAL_METRIC_VALUE = 0.9
-MODEL_PERFORMANCE_STORAGE = 'sfeda-mlops-processed'
+MODEL_PERFORMANCE_STORAGE = "sfeda-mlops-processed"
 
 
 def lambda_handler(event, context):
 
     container = "746614075791.dkr.ecr.us-west-1.amazonaws.com/sagemaker-xgboost:1.3-1"
 
-    best_training_job = event['BestTrainingJob']['TrainingJobName']
+    best_training_job = event["BestTrainingJob"]["TrainingJobName"]
     model_data_url = f"s3://sfeda-mlops-processed/processed_data/xgboost/{best_training_job}/output/model.tar.gz"
 
     # model_package_group_input_dict = {
@@ -28,55 +28,55 @@ def lambda_handler(event, context):
     # sagemaker.create_model_package_group(**model_package_group_input_dict)
 
     print("Additional step - register model to model group")
-    register_model_response = register_model(
-        container, model_data_url, best_training_job)
+    register_model_response = register_model(container, model_data_url, best_training_job)
 
     print("Save results of model (tunning job) to S3")
-    training_job_description = sagemaker.describe_training_job(
-        TrainingJobName=best_training_job)
-    objective_metric_value = [element for element in training_job_description['FinalMetricDataList']
-                              if element['MetricName'] == 'ObjectiveMetric'][0]['Value']
+    training_job_description = sagemaker.describe_training_job(TrainingJobName=best_training_job)
+    objective_metric_value = [
+        element
+        for element in training_job_description["FinalMetricDataList"]
+        if element["MetricName"] == "ObjectiveMetric"
+    ][0]["Value"]
 
     print("Check if new model is better than current one and update its status in model registry")
     if objective_metric_value < APPROVAL_METRIC_VALUE:
         DEPLOY = True
         model_package_update_input_dict = {
-            "ModelPackageArn": register_model_response['ModelPackageArn'],
-            "ModelApprovalStatus": "Approved"}
+            "ModelPackageArn": register_model_response["ModelPackageArn"],
+            "ModelApprovalStatus": "Approved",
+        }
         sagemaker.update_model_package(**model_package_update_input_dict)
     else:
         DEPLOY = False
         model_package_update_input_dict = {
-            "ModelPackageArn": register_model_response['ModelPackageArn'],
-            "ModelApprovalStatus": "Rejected"}
+            "ModelPackageArn": register_model_response["ModelPackageArn"],
+            "ModelApprovalStatus": "Rejected",
+        }
         sagemaker.update_model_package(**model_package_update_input_dict)
 
     print("Deploy model")
     if DEPLOY:
-        print('Creating model resource from training artifact...')
+        print("Creating model resource from training artifact...")
         create_model(best_training_job, container, model_data_url)
-        print('Creating endpoint configuration...')
+        print("Creating endpoint configuration...")
         create_endpoint_config(best_training_job)
-        print(
-            'There is no existing endpoint for this model. Creating new model endpoint...')
+        print("There is no existing endpoint for this model. Creating new model endpoint...")
         create_endpoint(endpoint, best_training_job)
-        event['endpoint'] = endpoint
-        event['stage'] = 'Deployment'
-        event['status'] = 'Creating'
-        event['message'] = 'Started deploying model "{}" to endpoint "{}"'.format(
-            best_training_job, endpoint)
+        event["endpoint"] = endpoint
+        event["stage"] = "Deployment"
+        event["status"] = "Creating"
+        event["message"] = 'Started deploying model "{}" to endpoint "{}"'.format(best_training_job, endpoint)
     else:
-        event['endpoint'] = endpoint
-        event['stage'] = 'Deployment'
-        event['status'] = 'Not deployed'
-        event['message'] = 'Model not deployed due to low metric value'
+        event["endpoint"] = endpoint
+        event["stage"] = "Deployment"
+        event["status"] = "Not deployed"
+        event["message"] = "Model not deployed due to low metric value"
     return event
 
 
 def register_model(container, model_data_url, best_training_job):
 
     modelpackage_inference_specification = {
-
         "InferenceSpecification": {
             "Containers": [
                 {
@@ -84,7 +84,9 @@ def register_model(container, model_data_url, best_training_job):
                 }
             ],
             "SupportedContentTypes": ["text/csv"],
-            "SupportedResponseMIMETypes": ["text/csv"], }}
+            "SupportedResponseMIMETypes": ["text/csv"],
+        }
+    }
 
     # Specify the model data
     modelpackage_inference_specification["InferenceSpecification"]["Containers"][0]["ModelDataUrl"] = model_data_url
@@ -92,16 +94,15 @@ def register_model(container, model_data_url, best_training_job):
     create_model_package_input_dict = {
         "ModelPackageGroupName": model_package_group_name,
         "ModelPackageDescription": best_training_job,
-        "ModelApprovalStatus": "PendingManualApproval"}
-    create_model_package_input_dict.update(
-        modelpackage_inference_specification)
-    create_mode_package_response = sagemaker.create_model_package(
-        **create_model_package_input_dict)
+        "ModelApprovalStatus": "PendingManualApproval",
+    }
+    create_model_package_input_dict.update(modelpackage_inference_specification)
+    create_mode_package_response = sagemaker.create_model_package(**create_model_package_input_dict)
     return create_mode_package_response
 
 
 def create_model(name, container, model_data_url):
-    """ Create SageMaker model.
+    """Create SageMaker model.
     Args:
         name (string): Name to label model with
         container (string): Registry path of the Docker image that contains the model algorithm
@@ -112,20 +113,17 @@ def create_model(name, container, model_data_url):
     try:
         sagemaker.create_model(
             ModelName=name,
-            PrimaryContainer={
-                'Image': container,
-                'ModelDataUrl': model_data_url
-            },
-            ExecutionRoleArn=EXECUTION_ROLE
+            PrimaryContainer={"Image": container, "ModelDataUrl": model_data_url},
+            ExecutionRoleArn=EXECUTION_ROLE,
         )
     except Exception as e:
         print(e)
-        print('Unable to create model.')
-        raise(e)
+        print("Unable to create model.")
+        raise (e)
 
 
 def create_endpoint_config(name):
-    """ Create SageMaker endpoint configuration.
+    """Create SageMaker endpoint configuration.
     Args:
         name (string): Name to label endpoint configuration with.
     Returns:
@@ -135,22 +133,17 @@ def create_endpoint_config(name):
         sagemaker.create_endpoint_config(
             EndpointConfigName=name,
             ProductionVariants=[
-                {
-                    'VariantName': 'prod',
-                    'ModelName': name,
-                    'InitialInstanceCount': 1,
-                    'InstanceType': INSTANCE_TYPE
-                }
-            ]
+                {"VariantName": "prod", "ModelName": name, "InitialInstanceCount": 1, "InstanceType": INSTANCE_TYPE}
+            ],
         )
     except Exception as e:
         print(e)
-        print('Unable to create endpoint configuration.')
-        raise(e)
+        print("Unable to create endpoint configuration.")
+        raise (e)
 
 
 def create_endpoint(endpoint_name, config_name):
-    """ Create SageMaker endpoint with input endpoint configuration.
+    """Create SageMaker endpoint with input endpoint configuration.
     Args:
         endpoint_name (string): Name of endpoint to create.
         config_name (string): Name of endpoint configuration to create endpoint with.
@@ -158,11 +151,8 @@ def create_endpoint(endpoint_name, config_name):
         (None)
     """
     try:
-        sagemaker.create_endpoint(
-            EndpointName=endpoint_name,
-            EndpointConfigName=config_name
-        )
+        sagemaker.create_endpoint(EndpointName=endpoint_name, EndpointConfigName=config_name)
     except Exception as e:
         print(e)
-        print('Unable to create endpoint.')
-        raise(e)
+        print("Unable to create endpoint.")
+        raise (e)
